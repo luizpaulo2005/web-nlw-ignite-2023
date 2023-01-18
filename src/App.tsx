2c5b0a52af0b9533123aaa62c4bb2df8e35c387d
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-01-17 22:17:13 -0300

    Segunda Aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { Habit } from "./components/Habit"
+// import { Habit } from "./components/Habit"
 
-function App() {
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+
+
+export function App() {
   return (
-    <div>
-      <Habit completed={2}/>
-      <Habit completed={3}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+
+      <Header/>
+      <SummaryTable/>
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
-
 
 // Componente: Reaproveitar / isolar
 // Propriedade: Uma informação enviada para modficar um componente visual ou comportamentalmente.
